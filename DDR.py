f58cf06af099579cb365a5c12b6caa818a300369
Thomas Ramapuram <thomas.ramapuram1@orangecounty.in>
2017-10-24 20:12:06 +0530

    Initial Commit

---
+++ DDR.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+import math
+import random
+
+# Initialize Pygame
+pygame.init()
+# Initiliace
+pygame.mixer.init()
+width, height = 640, 480
+screen=pygame.display.set_mode((width, height))
